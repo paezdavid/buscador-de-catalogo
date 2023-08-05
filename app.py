ae2d7ffec6d8367f130f9d3718ccf56d6e966d58
David Páez <davidgithub1@gmail.com>
2023-08-05 10:27:20 -0400

    added url links to search suggestions

--- app.py
+++ app.py
@@ -18,12 +18,24 @@ def suggest():
 
     conn = sqlite3.connect("example.db")
     cursor = conn.cursor()
-    cursor.execute(f"SELECT titulo FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
+    # cursor.execute(f"SELECT titulo FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
+
+    cursor.execute(f"SELECT titulo, url FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
     
     results = cursor.fetchall()
-    suggestions = [row[0] for row in results]
+    # suggestions = [row[0] for row in results]
+    suggestions = []
+    
+
+    for row in results:
+        suggestions.append({
+            "suggestion_title": row[0],
+            "suggestion_url": row[1]
+        })
     
     conn.commit() # Se committean los cambios a la DB
     conn.close()
+
+    print(suggestions)
 
     return suggestions
--- static/main.js
+++ static/main.js
@@ -17,7 +17,32 @@ function displaySuggestions(suggestions) {
   if (suggestions.length > 0) {
       suggestions.forEach(suggestion => {
         const li = document.createElement('li');
-        li.textContent = suggestion;
+        const a = document.createElement("a")
+        a.textContent = suggestion.suggestion_title;
+
+        const value = suggestion.suggestion_url;
+        const idPattern = /ln=\d+/; // Regular expression pattern to match 'ln=' followed by one or more digits
+        const biblioPattern = /document.resultados\.codbiblio\.value='([^']+)'/; // Regular expression pattern to match 'document.resultados.codbiblio.value=' followed by the desired value between single quotes
+        
+        const idMatch = value.match(idPattern);
+        const biblioMatch = value.match(biblioPattern);
+        let recordId = ""
+        let recordBiblioCode = ""
+
+        if (idMatch && biblioMatch) {
+          recordId = idMatch[0]; // The matched value will be stored in the first element of the match array
+          recordBiblioCode = biblioMatch[1]
+          console.log(recordId, recordBiblioCode); // Output: ln=206440727
+        } else {
+          console.log("No match found.");
+        }
+
+        // console.log(suggestion.suggestion_url.split(";"))
+
+        a.href = `https://www.cnc.una.py/opac/cliente.cgi?codbiblio=${recordBiblioCode}&mode=full&cclquery=${recordId}`
+
+        
+        li.appendChild(a)
         suggestionsList.appendChild(li);
       });
   } else {
