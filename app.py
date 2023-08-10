026f7c0420b60b0c4b7293255ffdf420100e9845
David Páez <davidgithub1@gmail.com>
2023-08-10 13:39:00 -0400

    added new route

--- app.py
+++ app.py
@@ -5,7 +5,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # conn = sqlite3.connect("catalogo_07_08_2023.db")
+    # cursor = conn.cursor()
+    # # cursor.execute(f"SELECT titulo FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
+
+    # cursor.execute(f"SELECT titulo, url FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
+    
+
+
     return render_template("index.html")
+
+
+@app.route("/registro/<id>")
+def datos_de_un_registro(id):
+    conn = sqlite3.connect("catalogo_07_08_2023.db")
+    cursor = conn.cursor()
+    # cursor.execute(f"SELECT titulo FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
+ 
+    cursor.execute(f"SELECT * FROM catalogo_03_08_2023 WHERE id = {id}") # Se trae un registro unico
+
+    registro_list_of_tup = cursor.fetchall() # Se almacena todo es una variable, pero fetchall() devuelve una lista de tuplas
+
+    registro_list = list(registro_list_of_tup[0]) # Convertimos en una lista la tupla que nos trajo fetchall()
+
+    print(registro_list)
+
+    return render_template("registro.html", registro_data=registro_list)
+    
+
 
 @app.route("/api/suggestions")
 def suggest():
@@ -16,11 +43,11 @@ def suggest():
         print(user_query)
         return json.dumps({"Error": "Query with empty string"})
 
-    conn = sqlite3.connect("example.db")
+    conn = sqlite3.connect("catalogo_07_08_2023.db")
     cursor = conn.cursor()
     # cursor.execute(f"SELECT titulo FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
 
-    cursor.execute(f"SELECT titulo, url FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
+    cursor.execute(f"SELECT id, titulo FROM catalogo_03_08_2023 WHERE titulo LIKE '{lowercase_input}%' LIMIT 10")
     
     results = cursor.fetchall()
     # suggestions = [row[0] for row in results]
@@ -29,8 +56,8 @@ def suggest():
 
     for row in results:
         suggestions.append({
-            "suggestion_title": row[0],
-            "suggestion_url": row[1]
+            "id": row[0],
+            "suggestion_title": row[1]
         })
     
     conn.commit() # Se committean los cambios a la DB
