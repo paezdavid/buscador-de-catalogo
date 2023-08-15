7561d78a12f377bcbb76a3fb252e89ac53e57a48
David Páez <davidgithub1@gmail.com>
2023-08-15 18:57:17 -0400

    added regex for string parsing

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template, request
-import sqlite3, json
+import sqlite3, json, re
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # conn = sqlite3.connect("catalogo_07_08_2023.db")
-    # cursor = conn.cursor()
-    # # cursor.execute(f"SELECT titulo FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
-
-    # cursor.execute(f"SELECT titulo, url FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
-    
-
 
     return render_template("index.html")
 
@@ -20,19 +13,23 @@ def index():
 def datos_de_un_registro(id):
     conn = sqlite3.connect("catalogo_07_08_2023.db")
     cursor = conn.cursor()
-    # cursor.execute(f"SELECT titulo FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
- 
+
     cursor.execute(f"SELECT * FROM catalogo_03_08_2023 WHERE id = {id}") # Se trae un registro unico
 
-    registro_list_of_tup = cursor.fetchall() # Se almacena todo es una variable, pero fetchall() devuelve una lista de tuplas
+    registro_list_of_tup = cursor.fetchall() # Se almacena todo en una variable, pero fetchall() devuelve una lista con una tupla dentro
 
     registro_list = list(registro_list_of_tup[0]) # Convertimos en una lista la tupla que nos trajo fetchall()
 
-    print(registro_list)
+    # Antes de enviar los datos al cliente, tenemos que parsear la lista de temas que viene envuelta en angle brackets (<INGENIERIA INDUSTRIAL>< CONSTRUCCION >)
+    angle_bracket_pattern = r'[<>]'
+    parsed_temas_str = re.sub(angle_bracket_pattern, "|", registro_list[6]) # reemplazamos los angle brackets por un |
+
+    # Reemplazamos el valor anterior del index de la lista en el cual se encuentran los temas 
+    registro_list[6] = parsed_temas_str.lower()
+
 
     return render_template("registro.html", registro_data=registro_list)
     
-
 
 @app.route("/api/suggestions")
 def suggest():
@@ -45,12 +42,10 @@ def suggest():
 
     conn = sqlite3.connect("catalogo_07_08_2023.db")
     cursor = conn.cursor()
-    # cursor.execute(f"SELECT titulo FROM catalogo WHERE titulo LIKE '\n\t\t{lowercase_input}%' LIMIT 10")
 
     cursor.execute(f"SELECT id, titulo FROM catalogo_03_08_2023 WHERE titulo LIKE '{lowercase_input}%' LIMIT 10")
     
     results = cursor.fetchall()
-    # suggestions = [row[0] for row in results]
     suggestions = []
     
 
